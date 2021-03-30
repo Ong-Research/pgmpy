08fb9804c512d4cf07146d4f4fcd87de4cd46058
nickpuntoforhof <nickpuntoforhof@gmail.com>
2021-03-29 22:03:40 -0500

    Noisy-Or parameter learning with leak node.

--- pgmpy/estimators/NoisyOrMLE.py
+++ pgmpy/estimators/NoisyOrMLE.py
@@ -13,26 +13,18 @@ from pgmpy.models import BayesianModel
 class NoisyOrMLE(ParameterEstimator):
     def __init__(self, model, data, **kwargs):
         """
-        Class used to compute parameters for a model using Maximum Likelihood Estimation.
+        Class used to compute parameters for a model using Maximum Likelihood
+        Estimation.
 
         Parameters
         ----------
         model: A pgmpy.models.BayesianModel instance
 
         data: pandas DataFrame object
-            DataFrame object with column names identical to the variable names of the network.
-            (If some values in the data are missing the data cells should be set to `numpy.NaN`.
-            Note that pandas converts each column containing `numpy.NaN`s to dtype `float`.)
-
-        state_names: dict (optional)
-            A dict indicating, for each variable, the discrete set of states
-            that the variable can take. If unspecified, the observed values
-            in the data set are taken to be the only possible states.
-
-        complete_samples_only: bool (optional, default `True`)
-            Specifies how to deal with missing data, if present. If set to `True` all rows
-            that contain `np.NaN` somewhere are ignored. If `False` then, for each variable,
-            every row where neither the variable nor its parents are `np.NaN` is used.
+            DataFrame object with column names identical to the variable names
+            of the network. (If some values in the data are missing the data
+            cells should be set to `numpy.NaN`. Note that pandas converts each
+            column containing `numpy.NaN`s to dtype `float`.)
 
         Examples
         --------
@@ -40,9 +32,13 @@ class NoisyOrMLE(ParameterEstimator):
         >>> import pandas as pd
         >>> from pgmpy.models import BayesianModel
         >>> from pgmpy.estimators import MaximumLikelihoodEstimator
-        >>> data = pd.DataFrame(np.random.randint(low=0, high=2, size=(1000, 5)),
-        ...                       columns=['A', 'B', 'C', 'D', 'E'])
-        >>> model = BayesianModel([('A', 'B'), ('C', 'B'), ('C', 'D'), ('B', 'E')])
+        >>> data = pd.DataFrame(np.random.randint(low=0,
+                                                  high=2,
+                                                  size=(1000, 5)),
+                                                  columns=['A', 'B', 'C',
+                                                           'D', 'E'])
+        >>> model = BayesianModel([('A', 'B'), ('C', 'B'),
+                                   ('C', 'D'), ('B', 'E')])
         >>> estimator = NoisyOrMLE(model, data)
         """
 
@@ -50,12 +46,12 @@ class NoisyOrMLE(ParameterEstimator):
             raise NotImplementedError(
                 "Noisy Or Estimate is only implemented for BayesianModel"
             )
-
         super(NoisyOrMLE, self).__init__(model, data, **kwargs)
 
     def get_parameters(self):
         """
-        Method to estimate the model parameters (CPDs) using Maximum Likelihood Estimation.
+        Method to estimate the model parameters (CPDs) using Maximum
+        Likelihood Estimation.
 
         Returns
         -------
@@ -74,12 +70,11 @@ class NoisyOrMLE(ParameterEstimator):
         >>> estimator = NoisyOrMLE(model, values)
         >>> estimator.get_parameters()
         """
-        parameters = []
 
+        parameters = []
         for node in sorted(self.model.nodes()):
             cpd = self.estimate_cpd(node)
             parameters.append(cpd)
-
         return parameters
 
     def estimate_cpd(self, node, leaky=False):
@@ -90,6 +85,10 @@ class NoisyOrMLE(ParameterEstimator):
         ----------
         node: int, string (any hashable python object)
             The name of the variable for which the CPD is to be estimated.
+
+        leaky: boolean
+            Indicates whether the leaky version of noisy-or will be used.
+            If true, a leaky parameter is estimated for null instances.
 
         Returns
         -------
@@ -110,36 +109,28 @@ class NoisyOrMLE(ParameterEstimator):
         | A(1) | 0.333333 |
         +------+----------+
         >>> cpd_C = NoisyOrMLE(model, data).estimate_cpd('C')
-        >>> print(cpd_C.inhibitor_probability)
-        [0.6666666666666666, 0.3333333333333333]
+        >>> print(cpd_C)
+        A    0.666667
+        B    0.333333
+        dtype: float64
         """
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("estimate_cpd: ", node)
-        parents = self.model.get_parents(node)
-        print("parents: ", parents)
 
         parent_cardinalities = []
         inhibitor_probability = []
+        parents = self.model.get_parents(node)
         node_cardinality = len(self.state_names[node])
-        # Extract state counts from data
         state_counts = self.state_counts(node)
+        # Add 1 to all cells with 0 counts to avoid divide by 0 errors.
         state_counts.loc[:, (state_counts == 0).all()] = 1
 
-        print('State Counts:')
-        print(state_counts)
-
-        # A Noisy-Or CPD cannot be fit to a node with no parents.
         # Learn a MLE CPT on nodes with no parents.
         if not parents:
             mle = MaximumLikelihoodEstimator(self.model, self.data)
             return mle.estimate_cpd(node)
-
-        # Calculate leak node
         if leaky:
             leak_matrix = state_counts.loc[:, (0,0)]
             leak_parameter = leak_matrix.loc[1] / leak_matrix.sum()
             inhibitor_probability.append(leak_parameter)
-
         for parent in parents:
             parent_cardinalities.append(len(self.state_names[parent]))
             # Calculate inhibitor probability
@@ -147,17 +138,6 @@ class NoisyOrMLE(ParameterEstimator):
             failures = count.iat[0,1]
             trials = count.sum(axis=0).iat[1]
             inhibitor_probability.append(failures / trials)
-
-        print('Node')
-        print(node)
-        print('Node Cardinality')
-        print(node_cardinality)
-        print('Parents')
-        print(parents)
-        print('Parent Cardinalities')
-        print(parent_cardinalities)
-        print('Inhibitor Probabilities')
-        print(inhibitor_probability)
 
         cpd = NoisyOrCPD(
             node,
@@ -167,5 +147,4 @@ class NoisyOrMLE(ParameterEstimator):
             inhibitor_probability=np.array(inhibitor_probability),
             leaky=leaky
         )
-
         return cpd
--- pgmpy/factors/discrete/NoisyOrCPD.py
+++ pgmpy/factors/discrete/NoisyOrCPD.py
@@ -2,97 +2,175 @@ from itertools import product
 from itertools import chain
 from warnings import warn
 import numbers
+import math
 
 import numpy as np
+import pandas as pd
 
 from pgmpy.factors.discrete import TabularCPD
 
 
 class NoisyOrCPD(TabularCPD):
-  """
-  Base class for Noisy-Or models.
-
-  This is an implementation of generalized Noisy-Or models and
-  is not limited to Boolean variables and also any arbitrary
-  function can be used instead of the boolean OR function.
-
-  Reference: http://xenon.stanford.edu/~srinivas/research/6-UAI93-Srinivas-Generalization-of-Noisy-Or.pdf
-  """
-
-  def __init__(
-        self,
-        variable,
-        variable_card,
-        evidence,
-        evidence_card,
-        inhibitor_probability,
-        leaky=False
-    ):
     """
-    Init method for NoisyOrModel.
+    CPD class for the Noisy-Or model.
 
-    Parameters
-    ----------
-    variable: int, string (any hashable python object)
-      The variable whose CPD is defined.
+    Model is restricted to boolean variables.
 
-    variable_card: integer
-      cardinality of variable
-
-    evidence: list, tuple, dict (array like)
-      array containing names of the evidence variables.
-
-    evidence_card: list, tuple, dict (array like)
-      array containing integers representing the cardinality
-      of the evidence variables.
-
-    inhibitor_probability: list, tuple, dict (array_like)
-      array containing the inhibitor probabilities of each variable.
-
-    leaky: boolean
-      boolean indicating if leaky null term is included in inhibitor
-      probabilities.
-
-    Examples
-    --------
-    >>> from pgmpy.models import NoisyOrModel
-    >>> model = NoisyOrModel('D', 2, ['A', 'B', 'C'], [2, 2, 2],
-                                                            [0.1,
-    ...                                                      0.1,
-    ...                                                      0.1])
+    Reference: http://xenon.stanford.edu/~srinivas/research/6-UAI93-Srinivas-Generalization-of-Noisy-Or.pdf
     """
-    self.inhibitor_probability = []
-    if len(evidence) != len(evidence_card):
-        raise ValueError("Size of evidence and evidence_card should be same")
-    elif (variable_card + leaky) != len(inhibitor_probability):
-        raise ValueError(
-            "Size of variables and inhibitor_probability should be same"
-        )
-    elif not all(
-        0 <= item <= 1 for item in inhibitor_probability
-    ):
-        raise ValueError(
-            "Probability values should be between 0 and 1(both inclusive)."
-        )
-    elif not isinstance(variable_card, int):
-        raise ValueError("Variable cardinality should be an integer")
-    else:
-        # Blank CPT added -- need to add
-        # functionality to convert to CPT later.
-        #if inhibitor_probability is not None:
-        self.inhibitor_probability.extend(inhibitor_probability)
-        # cpt = get_cpt(variable,
-        #     variable_card,
-        #     evidence,
-        #     evidence_card,
-        #     self.inhibitor_probability)
-        # else:
-        cpt = np.zeros((variable_card, np.product(evidence_card)))
 
-        super(NoisyOrCPD, self).__init__(
+    def __init__(
+            self,
             variable,
             variable_card,
-            cpt,
             evidence,
-            evidence_card
-        )
+            evidence_card,
+            inhibitor_probability,
+            leaky=False
+    ):
+        """
+        Init method for NoisyOrCPD.
+
+        Parameters
+        ----------
+        variable: int, string (any hashable python object)
+          The variable whose CPD is defined.
+
+        variable_card: integer
+          cardinality of variable
+
+        evidence: list, tuple, dict (array like)
+          array containing names of the evidence variables.
+
+        evidence_card: list, tuple, dict (array like)
+          array containing integers representing the cardinality
+          of the evidence variables.
+
+        inhibitor_probability: list, tuple, dict (array_like)
+          array containing the inhibitor probabilities of each variable.
+
+        leaky: boolean
+          boolean indicating if leaky null term is included in the model.
+
+        Examples
+        --------
+        >>> from pgmpy.models import NoisyOrModel
+        >>> model = NoisyOrModel('D', 2, ['A', 'B', 'C'], [2, 2, 2],
+                                                                [0.1,
+        ...                                                      0.1,
+        ...                                                      0.1])
+        """
+
+        if len(evidence) != len(evidence_card):
+            raise ValueError(
+                            'Size of evidence and'\
+                            'evidence_card should be same')
+        elif (len(evidence) + leaky) != len(inhibitor_probability):
+            raise ValueError(
+                'Size of variables and inhibitor_probability should be same'
+            )
+        elif not all(
+            0 <= item <= 1 for item in inhibitor_probability
+        ):
+            raise ValueError(
+                'Probability values should be between 0 and 1(both inclusive).'
+            )
+        elif not isinstance(variable_card, int):
+            raise ValueError('Variable cardinality should be an integer')
+        elif variable_card != 2:
+            raise ValueError('Child variable must be binary')
+        elif not all (
+            item == 2 for item in evidence_card
+        ):
+            raise ValueError('Parent variables must be binary')
+        else:
+            inhibitor_index = evidence.copy()
+            if leaky:
+                inhibitor_index.insert(0, 'leak')
+            self.inhibitor_probability = pd.Series(data=inhibitor_probability,
+                                                    index=inhibitor_index)
+            self.leaky = leaky
+            # Blank CPT added -- need to add
+            # functionality to convert to CPT later.
+            cpt = np.zeros((variable_card, np.product(evidence_card)))
+            super(NoisyOrCPD, self).__init__(
+                variable,
+                variable_card,
+                cpt,
+                evidence,
+                evidence_card
+            )
+    def __str__(self):
+        return str(self.inhibitor_probability)
+
+    def likelihood(self, data):
+        """
+        Calculates the likelihood of a single data observation given the model.
+
+        data : pandas Series object
+            A Series object with axis labels the same as variables in the
+            model.
+        """
+
+        inhibitor_probability = self.inhibitor_probability
+        child = self.variables[0]
+        parents = self.variables[1:]
+        leaky = self.leaky
+        child_data = data[child]
+        parent_data = data[parents]
+
+        if leaky:
+            # Extract leaky likelihood from inhibitor probabilities
+            p0 = inhibitor_probability[0]
+            inhibitor_probability = inhibitor_probability[1:]
+            def leaky_parent_likelihood(x, pi, p0=p0):
+                """Likelihood of single edge in Leaky Noisy-Or"""
+                if x == 0:
+                    return 1
+                else:
+                    return (1-pi) / (1-p0)
+            # Calculate product of inhibitor probabilities across positive
+            # instances in parent_data.
+            prod_term = parent_data.combine(inhibitor_probability,
+                                            leaky_parent_likelihood)
+            if child_data == 0:
+                return (1-p0) * np.prod(prod_term)
+            else:
+                return 1-(1-p0) * np.prod(prod_term)
+        else:
+            def parent_likelihood(x, pi):
+                """Likelihood of single edge in Noisy-Or"""
+                if x == 0:
+                    return 1
+                else:
+                    return 1 - pi
+            # Calculate product of inhibitor probabilities across positive
+            # instances in parent_data.
+            prod_term = parent_data.combine(inhibitor_probability,
+                                            parent_likelihood)
+            if child_data == 0:
+                return np.prod(prod_term)
+            else:
+                return 1 - np.prod(prod_term)
+
+    def get_row_likelihoods(self, data):
+        """
+        Calculates likelihood of each observation in the data given the model.
+
+        data : pandas DataFrame object
+            A DataFrame object with column names same as the variables
+            in the model.
+        """
+
+        return data.apply(self.likelihood, axis = 1)
+
+    def get_likelihood(self, data):
+        """Calculates likelihood of data given the model"""
+
+        return np.prod(self.get_row_likelihoods(data))
+
+    def get_log_likelihood(self, data):
+        """Calculates log likelihood of data given the model"""
+
+        ll = self.get_row_likelihoods(data).apply(math.log)
+        return np.sum(ll)
