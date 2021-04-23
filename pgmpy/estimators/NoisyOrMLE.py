c13c65486756ace6cac5fbe5be475ee0761f787f
nickpuntoforhof <nickpuntoforhof@gmail.com>
2021-04-23 12:53:38 -0500

    Added weights

--- pgmpy/estimators/NoisyOrMLE.py
+++ pgmpy/estimators/NoisyOrMLE.py
@@ -1,21 +1,19 @@
 # coding:utf-8
-
 from itertools import chain
-
 import numpy as np
+import pandas as pd
 import math
+
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
 
 from pgmpy.estimators import ParameterEstimator
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.factors.discrete.NoisyOrCPD import NoisyOrCPD
 from pgmpy.models import BayesianModel
 
-from scipy.optimize import minimize
-from scipy.optimize import Bounds
-
-
 class NoisyOrMLE(ParameterEstimator):
-    def __init__(self, model, data, **kwargs):
+    def __init__(self, model, data, weights=None, **kwargs):
         """
         Class used to compute parameters for a model using Maximum Likelihood
         Estimation.
@@ -50,6 +48,15 @@ class NoisyOrMLE(ParameterEstimator):
             raise NotImplementedError(
                 "Noisy Or Estimate is only implemented for BayesianModel"
             )
+        if not isinstance(data, pd.DataFrame):
+            raise ValueError(
+                "Data is not pandas DataFrame"
+            )
+        if weights is None:
+            self.weights = np.ones((data.shape[0], 1))
+        else:
+            weights.shape = (data.shape[0], 1)
+            self.weights = weights
         super(NoisyOrMLE, self).__init__(model, data, **kwargs)
 
     def get_parameters(self):
@@ -125,16 +132,18 @@ class NoisyOrMLE(ParameterEstimator):
         state_counts = self.state_counts(node)
         # Add 1 to all cells with 0 counts to avoid divide by 0 errors.
         state_counts.loc[:, (state_counts == 0).all()] = 1
-
+        X = np.array(self.data[parents])
+        y = np.array(self.data[node])
+        y.shape = (y.shape[0], 1)
+        w = self.weights
         # Learn a MLE CPT on nodes with no parents.
         if not parents:
             mle = MaximumLikelihoodEstimator(self.model, self.data)
             return mle.estimate_cpd(node)
         if leaky:
-            X = np.array(self.data[parents])
-            y = np.array(self.data[node])
-            y.shape = (y.shape[0], 1)
-            mle = fit_NoisyOr(X, y)
+            mle = fit_NoisyOr(X, y, w)
+        else:
+            mle = fit_NoisyOr(X, y, w)
 
         cpd = NoisyOrCPD(
             node,
@@ -154,32 +163,33 @@ def NoisyOrLogProb(D, theta):
     theta0 = theta[0]
     thetaI = theta[1:]
     y = D[0]
-    X = D[1:]
+    w = D[1]
+    X = D[2:]
     if y == 0:
-        return math.log(theta0) + sum(X*np.log(thetaI))
+        return w * (math.log(theta0) + sum(X*np.log(thetaI)))
     if y == 1:
-        return math.log(1 - (theta0 * np.prod(np.power(thetaI, X))))
+        return w * math.log(1 - (theta0 * np.prod(np.power(thetaI, X))))
     else:
         return ValueError
 
-def NoisyOrLL(theta, X, y):
+def NoisyOrLL(theta, X, y, w):
     """
     Noisy-Or Log Likelihood of parent data X and child data y.
     """
-    D = np.append(y, X, axis=1)
+    D = np.append(np.append(y, w, axis=1), X, axis=1)
     log_likelihoods = np.apply_along_axis(NoisyOrLogProb, 1, D, theta=theta)
     return -sum(log_likelihoods)
 
-def NoisyOrGradient(theta, X, y):
+def NoisyOrGradient(theta, X, y, w):
     """
     Noisy-Or Gradient of parent data X and child data y.
     """
     grad = np.zeros(theta.shape)
-    grad[0] = theta0Gradient(theta, X, y)
-    grad[1:] = thetaIGradient(theta, X, y)
+    grad[0] = theta0Gradient(theta, X, y, w)
+    grad[1:] = thetaIGradient(theta, X, y, w)
     return -grad
 
-def theta0Gradient(theta, X, y):
+def theta0Gradient(theta, X, y, w):
     """
     Noisy-Or leak node gradient.
     """
@@ -189,13 +199,15 @@ def theta0Gradient(theta, X, y):
     y1_idx = np.where((y == 1).all(axis=1))
     n_y0 = len(y0_idx[0])
     term1 = n_y0 / theta0
-    x_y1 = X[y1_idx]
+    X_y1 = X[y1_idx]
+    w_y1 = w[y1_idx]
     try:
-        term2 = np.apply_along_axis(theta0Quotient, 1, x_y1,
+        term2 = np.apply_along_axis(theta0Quotient, 1, X_y1,
                                     theta0=theta0, thetaI=thetaI)
-        return term1 - sum(term2)
+        term2 = sum(np.multiply(w_y1, term2))
+        return term1 - term2
     except ValueError:
-        print('Cannot apply_along_axis when any iteration dimensions are 0')
+        print('0 instances of y == 1 in data')
         print(x_y1)
 
 def theta0Quotient(x, theta0, thetaI):
@@ -206,7 +218,7 @@ def theta0Quotient(x, theta0, thetaI):
     denominator = (1 - theta0 * np.prod(np.power(thetaI, x)))
     return numerator / denominator
 
-def thetaIGradient(theta, X, y):
+def thetaIGradient(theta, X, y, w):
     """
     Gradient of Noisy-Or inhibitor parameters, excluding leak node parameter.
     """
@@ -224,6 +236,7 @@ def thetaIGradient(theta, X, y):
         x1_y1_idx = np.where(np.all(D == (1, 1), axis=1))
         n_x1_y0 = len(x1_y0_idx[0])
         x1_y1 = X[x1_y1_idx]
+        w_x1_y1 = w[x1_y1_idx]
 
         term1 = n_x1_y0 / thetai
         term2 = np.apply_along_axis(thetaIQuotient,
@@ -232,7 +245,8 @@ def thetaIGradient(theta, X, y):
                                     theta0=theta0,
                                     thetaI=thetaI,
                                     i=i)
-        grad.append(term1 - sum(term2))
+        term2 = sum(np.multiply(w_x1_y1, term2))
+        grad.append(term1 - term2)
     return grad
 
 def thetaIQuotient(x, theta0, thetaI, i):
@@ -245,7 +259,7 @@ def thetaIQuotient(x, theta0, thetaI, i):
     denominator = 1 - (theta0 * np.prod(np.power(thetaI, x)))
     return numerator / denominator
 
-def fit_NoisyOr(X, y):
+def fit_NoisyOr(X, y, w):
     """
     Learn NoisyOr parameters from MLE.
     Parameters
@@ -254,6 +268,8 @@ def fit_NoisyOr(X, y):
         Parent data
     y : numpy array
         Child data
+    w : numpy array
+        observation weights
 
     Returns
     -------
@@ -266,7 +282,7 @@ def fit_NoisyOr(X, y):
     bounds = Bounds(lb=0.001, ub=0.999)
     fit = minimize(NoisyOrLL,
                x0=thetaInit,
-               args=(X, y),
+               args=(X, y, w),
                method='L-BFGS-B',
                jac=NoisyOrGradient,
                bounds=bounds,
